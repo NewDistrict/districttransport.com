f8c95e6bf3902d4c2136b0c53c7c3cdf87dbfa30
brad winter <braddjwinter@gmail.com>
2015-08-07 10:02:39 -0700

    nd transport 2

--- signup.js
+++ signup.js
@@ -5,39 +5,27 @@ NDT.controller('signup', function($scope){
 
     $scope.greeting = 'hi';
 
-    $scope.message = 'message';
+    $scope.message = '';
+
+    $scope.signupVisible = false;
 
     $scope.accept = function(email) {
 
 
-        var yearOK = ($scope.year >= 1900 ) && ($scope.year <= 2015 );
+        if (!$scope.name) {
+            $scope.message = 'please enter your name';
 
-        if (yearOK && $scope.confirmed) {
+        } else if(!$scope.email || $scope.signup.EMAIL.$invalid) {
+            $scope.message = 'please enter a valid email';
 
-
-            $scope.message = '';
-
-            analytics.track('signed up to mailing list');
-
-            $scope.subscribed = true;
-            $scope.confirmationRed = false;
-
-
+        } else if (!$scope.winery) {
+            $scope.message = 'please enter your winery name';
         } else {
-            if (!$scope.name) {
-                $scope.message = 'please enter your name';
-
-            } else if(!$scope.email || $scope.signup.EMAIL.$invalid) {
-                $scope.message = 'please enter a valid email';
-
-            } else if (!$scope.winery) {
-                $scope.message = 'please enter your winery name';
-            } else {
-                $scope.message = 'great job!';
-
-            }
+            $scope.message = 'great job!';
 
         }
+
+
     };
 
 
